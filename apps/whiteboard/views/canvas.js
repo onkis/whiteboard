7690a40c5ba75ccad1d6346cb9efd7dab2bdfb12
Mike Ball <mike.ball3@gmail.com>
2011-01-15 23:28:48 -0500

    create awesome data

--- apps/whiteboard/views/canvas.js
+++ apps/whiteboard/views/canvas.js
@@ -14,9 +14,40 @@ Whiteboard.Canvas = SC.View.extend(
 /** @scope Whiteboard.Canvas.prototype */ {
   tagName: 'canvas',
   
+  lines: [],
+  
+  /*
+    keeps track of all the lines already drawn
+  */
+  _alreadyDrawn: {},
+  
+  _newLineAdded: function(){
+    this.displayDidChange();
+  }.observes('lines.[]'),
+  
   didCreateLayer: function(){
     var f = this.get('frame');
     this.$().attr({'width': f.width, 'height': f.height});
+  },
+  
+  render: function(context, firstTime){
+    if(!firstTime){
+      var ctx = this.$()[0].getContext("2d"), 
+          lines = this.get('lines'),
+          alreadyDrawn = this._alreadyDrawn;
+      lines.forEach(function(line){
+        if(!alreadyDrawn[line.guid]){
+          //TODO: add thickness and line color
+          ctx.moveTo(line.start.x, line.start.y);
+          
+          line.points.forEach(function(pt){
+            ctx.lineTo(pt.x,pt.y);
+          });
+          ctx.stroke();
+          alreadyDrawn[line.guid] = true;
+        }
+      });  
+    }
   },
   
   
@@ -31,26 +62,44 @@ Whiteboard.Canvas = SC.View.extend(
     });
     ctx.stroke();
   },
-  
+  // ..........................................................
+  // Events
+  // 
   mouseDown:function(evt){
-    this._startX = evt.pageX;
-    this._startY = evt.pageY;
-    this.lines = [];
+    var ctx = this._ctx || this.$()[0].getContext("2d"),
+        currentLine = this._currentLine;
+    
+    ctx.beginPath();
+    ctx.moveTo(evt.pageX,evt.pageY);
+    currentLine = {color:'', start: {x: evt.pageX, y: evt.pageY}, points: [], thickness:''};
+    SC.guidFor(currentLine);
+    this._alreadyDrawn[currentLine._guid] = true;
+    this._ctx = ctx;
+    this._currentLine = currentLine;
+    return YES;
+  },
+  mouseDragged: function(evt){
+    var ctx = this._ctx || this.$()[0].getContext("2d");
+    
+    //TODO: optimize by only adding if not the same as last point
+    ctx.lineTo(evt.pageX, evt.pageY);
+    this._currentLine.points.pushObject({x: evt.pageX, y: evt.pageY});
+    ctx.stroke();
+    
     return YES;
   },
   
   mouseUp: function(evt){
-    //this.draw();
-    return YES;
-  },
-  
-  lines: [],
-  
-  mouseDragged: function(evt){
-    this.lines.pushObject({x: evt.pageX, y: evt.pageY});
-    this.draw();
+    if(this._currentLine){
+      var lines = this.get('lines');
+      
+      lines.pushObject(this._currentLine);
+      this._currentLine = null;
+    }
     
     return YES;
   }
+    
+
 
 });
---
+++ data model moc.js
@@ -0,0 +1,13 @@
+{
+  guid: '',
+  activeCollaborators: [],
+  paths: [
+    {guid: '', color: '', start: {x,y}, points: [{x,y}], thickness: ''}
+  ]
+  
+}
+
+/*
+ Each path is created while is being drawin and is added to the data model 
+ once the mouseUp or touchesEnd occures 
+*/
